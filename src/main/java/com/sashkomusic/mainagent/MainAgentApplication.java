631abc9a4d4e14088a5465637d164fe6a5dd12a0
Sasha <sanykk2014@gmail.com>
2025-11-14 20:09:31 +0200

    init commit

---
+++ src/main/java/com/sashkomusic/mainagent/MainAgentApplication.java
@@ -0,0 +1,13 @@
+package com.sashkomusic.mainagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sashkomusic/mainagent/MainAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sashkomusic.mainagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
